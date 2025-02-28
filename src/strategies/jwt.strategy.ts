ca2ce6ebe9311e9a88b36aee2059ef92f5728373
MisterBarke <bachirmahbarke@gmail.com>
2025-02-28 11:06:12 +0100

    log in stratgy

--- src/strategies/jwt.strategy.ts
+++ src/strategies/jwt.strategy.ts
@@ -2,6 +2,8 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from 'src/app.module';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -11,8 +13,13 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       ignoreExpiration: false,
       secretOrKey: config.get<string>('JWT_SECRET_KEY'),
     });
-  }
+    
+
+  }                   
   async validate(payload: any) {
+    const app = await NestFactory.create(AppModule);
+    const configService = app.get(ConfigService);
+    console.log('JWT_SECRET in strategy.ts:', configService.get<string>('JWT_SECRET'));
     return { ...payload };
   }
-}
+}s
