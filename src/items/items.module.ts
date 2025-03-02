647e6667d9fefd2ec9eaf89e9ef30415fd63e838
MisterBarke <bachirmahbarke@gmail.com>
2025-03-02 02:36:04 +0100

    ajust

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ItemsController } from './items.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
-@Module({})
-export class ItemsModule {}
+@Module({
+    controllers: [ItemsController],
+    providers: [ItemsController],
+    imports:[PrismaModule]
+})
+export class ItemsModule {
+ 
+}
