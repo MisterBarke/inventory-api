bcdd7bafa72a4e452f3463bc0920c43c9dbe9847
MisterBarke <bachirmahbarke@gmail.com>
2025-03-02 06:42:54 +0100

    exporting item service

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ItemsController } from './items.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ItemsService } from './items.service';
 
 @Module({
     controllers: [ItemsController],
-    providers: [ItemsController],
+    providers: [ItemsService],
+    exports:[ItemsService],
     imports:[PrismaModule]
 })
 export class ItemsModule {
